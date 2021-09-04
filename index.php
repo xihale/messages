26666af01d970cfef5ff7343af70acc93a2cbb03
xihale <1745488156@qq.com>
2021-09-04 20:59:30 +0800

    修复了长文本检测的bug

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once "function.php";
 if(!empty($_GET)) {
 	$res = get(substr($_SERVER['REQUEST_URI'], 2));
 	if ($res == false) exit("短链无效");
-	if ($res['type'] == 0&&strpos($res['message'],'\n')==false) header("Location: " . (preg_match("/\bhttp[s*]:/",$res['message'])==false?"http://".$res['message']:$res['message']));
+	if ($res['type'] == 0&&strpos($res['message'],'\n')==-1) header("Location: " . (preg_match("/\bhttp[s*]:/",$res['message'])==false?"http://".$res['message']:$res['message']));
 	else if($res['type']==1){
 		header("Content-Type: text/plain");
 		echo $res['message'];
@@ -15,7 +15,7 @@ if(!empty($_GET)) {
 		<script src="https://cdn.jsdelivr.net/gh/xihale/lib@main/highlight/highlight.min.js"></script>
 		<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
 		<body>
-		<pre><code><?php echo preg_replace("/>/i","&gt;",preg_replace("/</i","&lt;",$res['message'])) ?></code></pre>
+		<pre><code style="font-size: 21px;"><?php echo preg_replace("/>/i","&gt;",preg_replace("/</i","&lt;",$res['message'])) ?></code></pre>
 		<script defer>
             hljs.initHighlightingOnLoad();
 		</script>
